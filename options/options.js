8d5378f063bdc690431d87af0a4fd151fea495e6
Crowdbotics <team@crowdbotics.com>
2022-05-27 13:08:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "simpay_35467",
+  url: "https://simpay_35467.botics.co",
+  api: "https://simpay_35467.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
